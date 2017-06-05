09c30f829e0c24db74571aff310f82d6d3228262
Mário Ribeiro <mail@marioribeiro.com>
2017-06-05 15:47:19 +0100

    Refactor to add isDivisible & isNotDivisible

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -3,16 +3,24 @@ function FizzBuzz() {
 
 FizzBuzz.prototype.play = function(number) {
   var output = "";
-  if (number % 3 == 0) {
+  if (this._isDivisibleBy(3, number)) {
     output += "Fizz"
   }
-  if (number % 5 == 0) {
+  if (this._isDivisibleBy(5, number)) {
     output += "Buzz"
   }
-  if (number % 3 != 0 && number % 5 != 0) {
+  if (this._isNotDivisibleBy(3, 5, number)) {
     output = number
   }
   return output
+}
+
+FizzBuzz.prototype._isDivisibleBy = function(divisor, number) {
+  return number % divisor === 0;
+}
+
+FizzBuzz.prototype._isNotDivisibleBy = function(divisor_1, divisor_2, number) {
+  return number % divisor_1 != 0 && number % divisor_2 != 0
 }
 
 for (i = 1; i <=1000; i++) {
